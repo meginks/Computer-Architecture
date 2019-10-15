1640ad0d844be0704e7332bb1ac1403618992274
meginks <meganjones.dev@gmail.com>
2019-10-15 15:40:05 -0500

    implemented load function

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -15,6 +15,7 @@ class CPU:
         self.MDR = 0 
         self.FL = 0 
 
+
     def ram_read(self, MAR):
         return self.ram[MAR] 
     
@@ -26,21 +27,27 @@ class CPU:
 
         address = 0
 
+        if len(sys.argv) < 2: 
+            print("You didn't give me a program name! I quit.")
+            sys.exit()
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ] 
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(sys.argv[1]) as f: 
+            for line in f: 
+                if line[0] != '#' and line != '\n':
+                    self.ram[address] = int(line[0:8], 2) 
+                    address += 1 
+            f.closed 
 
 
     def alu(self, op, reg_a, reg_b):
@@ -74,9 +81,9 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        HLT  = 0b00000001  
-        LDI  = 0b10000010  
-        PRN  = 0b01000111 
+        HLT = 0b00000001 
+        LDI = 0b10000010
+        PRN  = 0b01000111  
         running = True
         while running:
             IR = self.ram[self.pc]
